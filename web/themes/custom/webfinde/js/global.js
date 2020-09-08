546041f25e2d2fb1d6888d4fd8e11bcb2ab46ca3
IngJheyner <david-ibagon@hotmail.com>
2020-09-08 10:40:41 -0500

    Se completa y se agrega seccion de noticias, productos y servicios.

--- web/themes/custom/webfinde/js/global.js
+++ web/themes/custom/webfinde/js/global.js
@@ -7,8 +7,8 @@
 
     'use strict';
 
-    Drupal.behaviors.webfinde = {
-        attach: function(context, settings) {
+    //Drupal.behaviors.webfinde = {
+        //attach: function(context, settings) {
             
            /*===========================================
            BUSCADOR(HOME)
@@ -35,26 +35,52 @@
 
             $(".productosServicios .grid-container .grid-item").on('mouseover', function(){
 
-                $(this).children("figure").css({
-                    "height": "96%", 
+                $(this).children("figure").children(".fondo").css({
+                    "height": "400%", 
                     "transition" : ".5s all",
                     "background" : "#112d6a",
-                    "border-radius" : "0.8em"
+                    "border-radius" : "0.8em 0.8em 0.8em 0.8em"
                 });
+                
+                $(this).children("figure").children(".fondo").children("h1").children("button").show();
              
             });
 
             $(".productosServicios .grid-container .grid-item").on('mouseout', function(){
 
-                $(this).children("figure").css({
-                    "height": "25%", 
+                $(this).children("figure").children(".fondo").css({
+                    "height": "109%", 
                     "transition" : ".5s all",
-                    "background" : "repeating-linear-gradient(184deg, transparent 0, transparent 19%, #296294 0, #296294",
-                    "border-radius" : "0"});
+                    //"background" : "repeating-linear-gradient(184deg, transparent 0, transparent 19%, #296294 0, #296294",                   
+                    "border-radius": "0 0 0.8em 0.8em"});
+                
+                    $(this).children("figure").children(".fondo").children("h1").children("button").hide();
                 
             });
 
-        }
-    };
+            /* ===== ===== VENTANA MODAL ===== ===== */
+            $(".productosServicios .grid-container .grid-item figure button").on("click", function(){
 
-})(jQuery, Drupal);
+                $("#modalProductosServicios .modal-body h3").html($(this).attr('titulo'));
+                $("#modalProductosServicios .modal-body .descripcion").html($(this).attr('descripcion'));
+                //$("#modalProductosServicios img").attr("src", $(this).attr('imagen'));
+                $("#modalProductosServicios").css({
+                    "background-image":"linear-gradient(to bottom, transparent 193px,#112d6a 0,#112d6a),url('"+$(this).attr('imagen')+"')"
+                });
+
+                if($(this).attr('url') != ""){
+
+                    $("#modalProductosServicios .modal-body a").show();
+                    $("#modalProductosServicios .modal-body a").attr("href", $(this).attr('url')).html($(this).attr('tituloUrl'));
+
+                }else{
+
+                    $("#modalProductosServicios .modal-body a").hide();
+
+                }    
+            });
+
+        //}
+    //};
+
+})(jQuery);
