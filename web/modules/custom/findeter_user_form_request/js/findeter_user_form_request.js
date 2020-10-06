a490b2f2b3532a7e8167b234a27ba50c59f167ff
Sandi Aramayo <sandiaras@MacBook-Pro-de-Sandi.local>
2020-10-06 14:51:15 -0400

    Findeter URF - adding library and js/css files

---
+++ web/modules/custom/findeter_user_form_request/js/findeter_user_form_request.js
@@ -0,0 +1,61 @@
+(function ($, Drupal) {
+  //'use strict';
+  Drupal.behaviors.findeterUserFormRequest = {
+    attach: function (context, settings) {
+      
+      if($('#edit-field-request-description').length){
+        $("#edit-field-request-description").keyup(function() {
+          var sizeText = $('#edit-field-request-description').val().length;
+          $('.counter-char').html(sizeText);
+        });
+      }
+
+      if($('#field-contact-answer-channel-anonimous').length){
+        $('#no-anonimous').hide();
+
+
+        $('input[type=radio][name=field_contact_answer_channel_anonimous]').change(function() {
+          if (this.value == '1') {
+            $('#no-anonimous').hide();
+          }
+          else if (this.value == '0') {
+            $('#no-anonimous').show();
+          }
+        });
+
+      }
+
+      $.fn.afterAsignCallback = function(argument) {
+        console.log('afterAsignCallback is called.');
+        location.reload();
+      };
+
+      //var abc = $('#edit-field-age-range').selectToUISlider().next();
+
+      /*$(".htc-ajax-form").on("click",function(){
+
+        var ajaxSettings = {
+          url: Drupal.url("htc-ajax-response"),
+          dialogType: 'ajax',
+        };
+
+        var myAjaxObject = Drupal.ajax(ajaxSettings);
+
+        // Declare a new Ajax command specifically for this Ajax object.
+        myAjaxObject.commands.insert = function (ajax, response, status) {
+
+          console.info('response', response);
+          $("#htcp-wrapper").html(response.data);
+          Drupal.attachBehaviors(document, Drupal.settings);
+
+        };
+
+        // Programmatically trigger the Ajax request.
+        myAjaxObject.execute();
+
+      });*/
+
+
+    }
+  };
+})(jQuery, Drupal);
