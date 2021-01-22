62db67820668183906a176be3a2200cf1ede7cf8
IngJheyner <david-ibagon@hotmail.com>
2021-01-22 17:05:00 -0500

    Cambios en pruebas

--- web/sites/default/soapFindeterconsumir/controller/CredencialController.php
+++ web/sites/default/soapFindeterconsumir/controller/CredencialController.php
@@ -5,9 +5,11 @@ function consult(){
         $url = "http://10.10.3.22:6525/SIAAF_ServiciosWCF/Servicios/SvcCredenciales.svc?WSDL";
         $client = new SoapClient($url);
         //$result = $client->resultado_xml([ "IDUnico" => "651551E9-814E-40BE-9590-775C3F59A0F0"]);
-        $result = $client->ConsultarCredencial([ "idCredencial" => "651551E9-814E-40BE-9590-775C3F59A0F0"]);
+        $result = $client->ConsultarCredencial([ "idCredencial" => "f7f81cd6-5384-4da6-bba0-0d4ca4a321b3"]);
         $result = $result->ConsultarCredencialResult;
-        echo base64_encode($result->Clave);
+        $result = base64_decode($result->Clave);
+
+        echo json_encode($result);
         //echo json_encode($client->__getFunctions());
     } catch ( SoapFault $e ) {
         echo $e->getMessage();
