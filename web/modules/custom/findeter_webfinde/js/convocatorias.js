35061575aadd8107e012cd66055022a7ece07188
IngJheyner <david-ibagon@hotmail.com>
2021-10-27 13:24:21 -0500

    Config. site y ruta en convocatorias

--- web/modules/custom/findeter_webfinde/js/convocatorias.js
+++ web/modules/custom/findeter_webfinde/js/convocatorias.js
@@ -17,15 +17,15 @@
                 $("form#node-convocatorias-form input[id='edit-field-convcatoria-archivos-entity-browser-entity-browser-open-modal'], form#node-convocatorias-edit-form input[id='edit-field-convcatoria-archivos-entity-browser-entity-browser-open-modal'] ").on('click', function() {
 
                     codigoProceso = $("form#node-convocatorias-form input[id='edit-title-0-value'],form#node-convocatorias-edit-form input[id='edit-title-0-value']").val();
-
+                    
                     $(document, context).ajaxStop(function() {
 
                         let iframe = '';
 
                         document.querySelector('iframe#entity_browser_iframe_entity_browser_archivos_convocatorias').onload = function(e) {
-
+                            
                             iframe = document.querySelector('iframe#entity_browser_iframe_entity_browser_archivos_convocatorias').contentWindow.document;
-
+                            
                             let files = iframe.getElementById('ief-dropzone-upload');
                             $(files).wrap("<details class='required-fields field-group-details js-form-wrapper form-wrapper seven-details' data-drupal-selector='edit-group-file-conv' id='edit-group-file-conv'><div class='seven-details__wrapper details-wrapper'></div></details>");
                             $(files).parent().before("<summary role='button' aria-controls='edit-group-prueba' aria-expanded='false' aria-pressed='false' class='seven-details__summary form-required'><span>Expandir formularios para los archivos cargados.</span></summary>");
@@ -36,13 +36,15 @@
 
                             $(uploadFile).on('click', function() {
                                 iframe.getElementById('edit-group-file-conv').removeAttribute('open');
+                                
                             });
 
                             iframe.getElementById('edit-group-file-conv').addEventListener('click', function() {
                                 let newFiles = iframe.getElementById('ief-dropzone-upload');
+                                console.log(newFiles);
                                 $(newFiles).children('div.form-wrapper').each(function(idx, el) {
-                                    $(el).children('fieldset').children('div.fieldset-wrapper').children('div.field--name-field-convcatoria-ruta-archivo').children().children('input').val(codigoProceso);
-                                    $(el).children('fieldset').children('div.fieldset-wrapper').children('div.field--name-field-convcatoria-ruta-archivo').css('display', 'none');
+                                    $(el).children('fieldset').children('div.fieldset__wrapper').children('div.field--name-field-convcatoria-ruta-archivo').children().children('input').val(codigoProceso);
+                                    $(el).children('fieldset').children('div.fieldset__wrapper').children('div.field--name-field-convcatoria-ruta-archivo').css('display', 'none');
                                 });
                             });
 
