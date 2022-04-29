98c9997c0670dc4cb6f39c0196db5f805e60dc95
IngJheyner <david-ibagon@hotmail.com>
2022-04-29 09:11:54 -0500

    Conexion a login API

--- web/modules/custom/findeter_pqrsd/src/ApiSmfc.php
+++ web/modules/custom/findeter_pqrsd/src/ApiSmfc.php
@@ -1,48 +1,61 @@
 <?php
 namespace Drupal\findeter_pqrsd;
 
-use Drupal\Component\Utility\Crypt;
-use Drupal\Component\Serialization\Json;
+use Drupal\Core\Http\ClientFactory;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+
+
+use Drupal\findeter_pqrsd\ApiSmfcAbs;
 /**
- * @file
  * Service used to create a client web service for the SMFC REST API
+ * @see Drupal\findeter_pqrsd\ApiSmfcAbs
+ * @param $credentials
+ *  A fix that brings the login credentials and uri to connect to the API
+ *   - user: user value chain
+ *   - pass: pass password value chain
+ *   - secret_key: secret key value chain
+ *   - uri: Url https SMFC
  */
 
-class ApiSmfc{
+class ApiSmfc extends ApiSmfcAbs{
 
     protected $credentials;
 
     /**
      * @param $credentials
-     * 
+     * @param Drupal\Core\Http\ClientFactory $http_client_factory
      */
 
-    public function __construct($credentials)
-    {
+    public function __construct($credentials, ClientFactory $http_client_factory, LoggerChannelFactoryInterface $logger){
 
-        $this->credentials = $credentials;
+        $this->credentials = $credentials;        
+        $this->secretKey = $this->credentials[0]["secret_key"];
+        $this->uri = $this->credentials[0]["uri"];
 
-        $user = $this->credentials[0]["user"];
-        $pass = $this->credentials[0]["pass"];
-        $secret_key = $this->credentials[0]["secret_key"];
-
-        $credentials = ["username" => "$user", "password" => "$pass"];
-
-        $data = json_encode($credentials);
-
-        $hmac = hash_hmac('sha256', utf8_encode($data), utf8_encode($secret_key),FALSE);
-
-        $base =strtoupper($hmac);
+        //Services
+        $this->httpClientFactory = $http_client_factory;
+        $this->logger = $logger;
         
-        //$hmacc = strtoupper(str_replace(['+', '/', '='], ['-', '_', ''], $base_64));
-        
+        $this->login();
+       
     }
+
     /**
-     * 
+     * @inheritDoc
      */
-    private function signature(){
-          
-    
+    public function login()
+    {
+        $user = $this->credentials[0]["user"];
+        $pass = $this->credentials[0]["pass"]; 
+
+        $data = '{"username": "'.$user.'", '.
+                '"password": "'.$pass.'"}';        
+        
+        $signature = $this->signature($data);
+
+        $tokens = $this->httpClient($signature, $data, 'login/');
+
+       
     }
     
 }
